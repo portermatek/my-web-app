753b14d3e32fe63c76bfb08d9a4caf32b9fe6217
portermatek <portermatek@gmail.com>
2025-05-05 14:26:34 +0200

    Switch to HashRouter for GitHub Pages compatibility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ IMPORT THIS
+import { HashRouter } from 'react-router-dom'; // ✅ use HashRouter for GitHub Pages
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ WRAP HERE */}
+    <HashRouter> {/* ✅ GitHub Pages works with HashRouter */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
